a1377e302e83042a5bd5fd0fa0581f342b3d06c0
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-07-27 19:28:14 +0900

    [Feat] 페이지 라우팅

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,42 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import NotFound from "./NotFound";
 import GlobalStyle from "./styles/GlobalStyle.ts";
+import Login from "./pages/user/Login.tsx";
+import Home from "./Home.tsx";
+import Signup from "./pages/user/Signup.tsx";
+import UserAnnual from "./pages/user/UserAnnual.tsx";
+import UserInfo from "./pages/user/UserInfo.tsx";
+import AdminLogin from "./pages/admin/AdminLogin.tsx";
+import Management from "./pages/admin/Management.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Login /> },
+      {
+        path: "/main",
+        element: <Home />,
+      },
+      { path: "/signup", element: <Signup /> },
+      { path: "/userinfo", element: <UserInfo /> },
+      { path: "/userannual", element: <UserAnnual /> },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <AdminLogin /> },
+      { path: "/admin/main", element: <Home /> },
+      {
+        path: "/admin/management",
+        element: <Management />,
+      },
+    ],
   },
 ]);
 
---
+++ src/pages/user/UserAnnual.tsx
@@ -0,0 +1,3 @@
+export default function UserAnnual() {
+  return <div>UserAnnual</div>;
+}
